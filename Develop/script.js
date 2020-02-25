02575fef71472770743cecb2a443ae089f41bb2a
Timothy L <talmabertt@gmail.com>
2020-02-24 18:29:26 -0600

    updated multiple things in html and javascrip

--- Develop/script.js
+++ Develop/script.js
@@ -13,4 +13,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", btn);
---
+++ Develop/script2.js
@@ -0,0 +1,91 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+
+}
+
+// Generates password
+function generatePassword() {
+    // Establishing variables for generatePassword function  
+    var allPossibleChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()-_=+\|]}[{/?.>,<";  
+    var passwordPool = "";
+    var completedPassword ="";
+    // User decision inputs
+    var lowerCase= confirm("Do you want lower case letters included?");
+    var upperCase = confirm("Do you want upper case letters included?");
+    var numbers = confirm("Do you want numbers included?");
+    var specialCharacters = confirm("Do you want special characters included?");
+  
+
+    //set password length/complexity
+    let complexity = document.getElementById("slidertool").value;
+
+
+    //set default length display of 25
+    document.getElementById("length").innerHTML = "Length: 25";
+
+    //function to set length based on slider position
+    document.getElementById("slidertool").oninput = function(){
+
+    if(document.getElementById("slidertool").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slidertool").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+    }
+
+      // User input determining length of password, converted to a number format variable if it is in range
+      if (charCount >=8 && charCount <=128) {
+          var charCountValue = parseInt(charCount);
+      }
+      // If user selects for lower case characters, adds lower case characters to password pool
+      if (lowerCase === true) {
+          var spliceLowerCase = allPossibleChar.slice(26, 52);
+          passwordPool = passwordPool.concat(spliceLowerCase);
+          console.log(passwordPool);
+      }
+      // If user selects for upper case characters, adds upper case characters to password pool
+      if (upperCase === true) {
+          var spliceUpperCase = allPossibleChar.slice(0, 26);
+          passwordPool = passwordPool.concat(spliceUpperCase);
+          console.log(passwordPool);
+      }
+      // If user selects for numbers, adds numbers to password pool
+      if (numbers === true) {
+          var spliceNumbers = allPossibleChar.slice(52, 62);
+          passwordPool = passwordPool.concat(spliceNumbers);
+          console.log(passwordPool);
+      }
+      // If user selects for special characters, adds special characters to password pool
+      if (specialCharacters === true) {
+          var spliceSpecialCharacters = allPossibleChar.slice(62, 91);
+          passwordPool = passwordPool.concat(spliceSpecialCharacters);
+          console.log(passwordPool);
+      }
+      // Concatenating random characters from password pool and forming random password
+      for (i = 0; i < charCountValue; i++) {
+          var randomPasswordPool = passwordPool[Math.floor(Math.random() * passwordPool.length)];
+          var completedPassword = completedPassword.concat(randomPasswordPool);
+      }
+      function getCharacterLength (str) {
+          return [...str].length;
+      }
+      // Logging to ensure correct length
+      console.log(getCharacterLength(completedPassword));
+      // Setting fallback in case user selects no characters
+      if (lowerCase === false && numbers === false && upperCase === false && specialCharacters === false) {
+          alert("You must select at least one character option. Please try again.");
+          return completedPassword = "Try Again."
+      }
+      return completedPassword;
+  } 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
